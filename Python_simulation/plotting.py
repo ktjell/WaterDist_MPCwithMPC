1f86bd0ae6153653ec658978f04840e94234ec64
ktjell <kst@es.aau.dk>
2023-06-13 10:17:05 +0200

    scipy_separable_up

--- Python_simulation/plotting.py
+++ Python_simulation/plotting.py
@@ -25,11 +25,11 @@ class plotting:
         
         self.f, self.ax = plt.subplots(4, sharex=True, figsize = (15,9))
         plt.xticks(rotation=45)
-        loc = md.AutoDateLocator(interval_multiples=True) # this locator puts ticks at regular intervals
-        # self.ax[0].xaxis.set_major_formatter(md.ConciseDateFormatter(loc)) #md.DateFormatter('%H:%M'))
-        self.ax[0].xaxis.set_major_locator(loc)
-        minloc = md.AutoDateLocator(minticks=2, maxticks=5)
-        self.ax[0].xaxis.set_minor_locator(minloc)
+        # loc = md.AutoDateLocator(interval_multiples=True) # this locator puts ticks at regular intervals
+        # # self.ax[0].xaxis.set_major_formatter(md.ConciseDateFormatter(loc)) #md.DateFormatter('%H:%M'))
+        # self.ax[0].xaxis.set_major_locator(loc)
+        # minloc = md.AutoDateLocator(minticks=2, maxticks=5)
+        # self.ax[0].xaxis.set_minor_locator(minloc)
         for ax in self.ax:
             ax.grid(axis = 'x')
 
