ceb008f25717f64ce95669860ee04fc28b8153b7
ktjell <kst@es.aau.dk>
2023-06-15 14:22:37 +0200

    corrections

--- RPI_simulation/local_ctr.py
+++ RPI_simulation/local_ctr.py
@@ -50,81 +50,72 @@ class loc_ctr(Thread):
 
     def run(self):
         print('Local controller ', self.p_nr+1, ' online')
-        # self.startSolver()
-        # print('Solver succesfully started.')
-        # ite = 100
-        # Qextr = np.zeros((simu.M))
-        # Uglobal = np.zeros((simu.M,simu.N))
+        self.startSolver()
+        print('Solver succesfully started.')
         
-        # lamb = np.zeros((ite+1, simu.M, simu.N))
-        # x0 = np.zeros((2*simu.M)) 
-        # u = np.zeros((ite))
-        # rho = .8
-        # j = 0
-        u = 30
+        Qextr = np.zeros((simu.M))
+        Uglobal = np.zeros((simu.M,simu.N))
+        ite = 100
+        lamb = np.zeros((ite+1, simu.M, simu.N))
+        u = 0#np.zeros((simu.ite))
+        rho = .8
+        j = 0
         for k in range(simu.ite):
             
             #get data
             h = self.com_func.get_data(str(k), 1)[0] #Get tank level (will later be sensor measurement)
             print('h: ', h)
-            U = np.ones((2,2))*k
-            self.distribute_shares(str(k), U)
-            Usum = self.reconstruct_secret(str(k))
-            print('reconstructed secret:')
-            print(Usum)
             
-            if h >= tank.hmax:
-                u = 0
-            elif h <= tank.hmin:
-                u = 80
             
-            # #Reset lambda and use the last lambda from the previous round 
-            # lamb_temp = lamb[j-1,:,:]
-            # lamb = np.zeros((ite+1, simu.M, simu.N))
-            # lamb[0,:,:] = lamb_temp
-            # Qextr[k%simu.M] = u    
-            # #Set iteration variables
-            # acc = True
-            # j = 0
-            # while acc and j < ite:
-                #Solve the local opti problem
-                    
-                # par = simu.c[k:k+simu.M].flatten().tolist()
-                # par.extend(simu.d[k:k+simu.M].flatten().tolist())
-                # par.extend(Qextr.flatten().tolist())
-                # par.extend(lamb[j,:,0]).flatten().tolist()
-                # par.extend(lamb[j,:,1]).flatten().tolist()
-                # par.extend(Uglobal[:,0]).flatten().tolist()
-                # par.extend(Uglobal[:,1]).flatten().tolist()
-                # par.append(h[k])
-                # par.append(rho)
-                # response =  self.mng.call(par)
-                # if response.is_ok():
-                #     # Solver returned a solution
-                #     solution_data = response.get()
-                #     s = solution_data.solution
-                #     U[:,0] = s[:simu.M]
-                #     U[:,1] = s[simu.M:]
-                # else:
-                #     print('opti error')
-                #     break
+            #Reset lambda and use the last lambda from the previous round 
+            lamb_temp = lamb[j-1,:,:]
+            lamb = np.zeros((ite+1, simu.M, simu.N))
+            lamb[0,:,:] = lamb_temp
+            Qextr[(k%simu.M)-1] = u
+            Qex = np.cumsum(Qextr[::-1])[::-1]
+            #Set iteration variables
+            acc = True
+            j = 0
+            U = np.zeros((simu.M, simu.N))
+            while acc and j < ite:
+                # Solve the local opti problem
+                par = simu.c[k:k+simu.M].flatten().tolist()
+                par.extend(simu.d[k:k+simu.M].flatten().tolist())
+                par.extend(Qex.flatten().tolist())
+                par.extend(lamb[j,:,0]).flatten().tolist()
+                par.extend(lamb[j,:,1]).flatten().tolist()
+                par.extend(Uglobal[:,0]).flatten().tolist()
+                par.extend(Uglobal[:,1]).flatten().tolist()
+                par.append(h[k])
+                par.append(rho)
+                response =  self.mng.call(par)
+                if response.is_ok():
+                    # Solver returned a solution
+                    solution_data = response.get()
+                    s = solution_data.solution
+                    U[:,0] = s[:simu.M]
+                    U[:,1] = s[simu.M:]
+                else:
+                    print('opti error')
+                    break
                 
                 
 
-            #     #Value to send to cloud to compute sum
-            #     to_sum = U + 1/rho * lamb[j,:,:]
-            #     self.distribute_shares(str(k), to_sum)
-            #     #Get sum of local U's back from cloud and compute Uglobal as the average. 
-            #     Usum = self.reconstruct_secret(str(k))
+                #Value to send to cloud to compute sum
+                to_sum = U + 1/rho * lamb[j,:,:]
+                self.distribute_shares(str(k), to_sum)
+                #Get sum of local U's back from cloud and compute Uglobal as the average. 
+                Usum = self.reconstruct_secret(str(k))
                 
-            #     Uglobal = (1/simu.N) * Usum
+                Uglobal = (1/simu.N) * Usum
                 
-            #     #Update local lambda
-            #     lamb[j+1,:,:] = lamb[j,:,:] + rho*( U - Uglobal )
-            #     #Compute accuracy of lambda
-            #     acc = (np.linalg.norm(lamb[j,:,:] - lamb[j-1,:,:], 2) > 0.1)  
-            #     #Update j
-            #     j+=1  
+                #Update local lambda
+                lamb[j+1,:,:] = lamb[j,:,:] + rho*( U - Uglobal )
+                #Compute accuracy of lambda
+                acc = (np.linalg.norm(lamb[j,:,:] - lamb[j-1,:,:], 2) > 0.1)  
+                #Update j
+                j+=1  
+                u = U[0,self.p_nr]
             
             #Send the computed u to simulator (will later be input to local pump)
             self.com_func.broadcast_data(u, str(k), ips.addr_dict['simulator'])
