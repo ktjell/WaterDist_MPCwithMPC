3d33dabe223864ec835901aeb192028c99a5f773
ktjell <kst@es.aau.dk>
2023-07-03 13:01:26 +0200

    rpi update

--- RPI_simulation/local_ctr.py
+++ RPI_simulation/local_ctr.py
@@ -85,6 +85,7 @@ class loc_ctr(Thread):
             j = 0
             U = np.zeros((simu.M, simu.N))
             while acc and j < ite:
+                print(j)
                 # Solve the local opti problem
                 par = simu.c[k:k+simu.M].flatten().tolist()
                 par.extend(simu.d[k:k+simu.M].flatten().tolist())
