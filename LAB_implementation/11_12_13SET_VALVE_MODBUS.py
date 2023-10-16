9ca89c18b63456f5944eb2bcc00f6fa17b96a174
ktjell <kst@es.aau.dk>
2023-10-16 10:43:28 +0200

    update RPI_sim

---
+++ LAB_implementation/11_12_13SET_VALVE_MODBUS.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 10:42:10 2023
+
+@author: kst
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 10:13:36 2023
+
+@author: kst
+"""
+
+from pyModbusTCP.client import ModbusClient
+
+c = ModbusClient(host = 'localhost', port = 503, unit_id = 15, auto_open = True)
+if c.open():
+    print('Modbus client succesfully connected')
+else:
+    print('Modbus client connection failed.')
+    
+on = 100*100
+    
+c.write_multiple_registers(1, [on])
+c.write_multiple_registers(2, [on])
+c.write_multiple_registers(3, [on])
