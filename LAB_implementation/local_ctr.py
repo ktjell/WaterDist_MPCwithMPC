0cb6e5e92082e03f49bd432d3568e5e41fb82e1b
ktjell <kst@es.aau.dk>
2023-10-16 09:21:58 +0200

    update RPI_sim

--- LAB_implementation/local_ctr.py
+++ LAB_implementation/local_ctr.py
@@ -67,7 +67,7 @@ class loc_ctr(Thread):
             h = c_tank.read_input_registers(7, 1)[0]
                 
             print('h: ', h)
-            # h = h/100
+            h = h/100
             
             #Reset lambda and use the last lambda from the previous round 
             lamb_temp = lamb[j-1,:,:]
