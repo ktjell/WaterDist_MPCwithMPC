55c1e44d9744c4c6af790e2495107acc11303ca1
ktjell <kst@es.aau.dk>
2023-11-06 10:24:31 +0100

    update LAB_sim

--- RPI_simulation/ip_config.py
+++ RPI_simulation/ip_config.py
@@ -10,17 +10,17 @@ class ipconfigs:
     port = 62
     
     local_ctr_addr = [
-                      ['192.168.100.41', 62], #party 0: Local ctr 1
-                      ['192.168.100.43', 62]  #party 1: Local ctr 2
+                      ['192.168.100.1', 62], #party 0: Local ctr 1
+                      ['192.168.100.2', 62]  #party 1: Local ctr 2
                       ]
     
     simulator_addr = [
-                     ['192.168.100.31', 62]   #Simulator
+                     ['192.168.100.3', 62]   #Simulator
                      ]
     
     cloud_addr = [
-                  ['192.168.100.1', 62], #party 0: Cloud 1
-                  ['192.168.100.2', 62], #party 1: Cloud 2
+                  ['192.168.100.5', 62], #party 0: Cloud 1
+                  ['192.168.100.6', 62], #party 1: Cloud 2
                   # ['192.168.100.6', 62]  #-
                   ]
     addr_dict = {'local_ctr':local_ctr_addr, 'simulator':simulator_addr, 'cloud':cloud_addr}
